602d4e8a29e915685dbad1f4d034a1ac336c4c99
John Loberger <johnloberger@gmail.com>
2020-04-28 21:22:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  fresh_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        if !fresh_hash[name]
+          fresh_hash[name] = {}
+        end
+        if !fresh_hash[name][key]
+          fresh_hash[name][key] = []
+        end
+        fresh_hash[name][key] << new_value.to_s
+      end
+    end
+  end
+  fresh_hash
 end
